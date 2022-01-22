c3676c088147b3bb44b129b2fce5e548672fb413
Görkem Duymaz <gorkem.duymaz@useinsider.com>
2022-01-23 00:12:32 +0300

    Typo

--- src/Enums/TrackerEnum.php
+++ src/Enums/TrackerEnum.php
@@ -2,10 +2,11 @@
 
 namespace Dawnstar\Tracker\Enums;
 
-
+/**
+ * Class TrackerEnum
+ * @package Dawnstar\Tracker\Enums
+ */
 final class TrackerEnum
 {
     const ROBOT = 'robot';
-    const TRACKER_TYPE_SESSION = 'session';
-    const TRACKER_TYPE_COOKIE_SESSION = 'cookie_session';
 }
